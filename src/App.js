9b72467ad1cecff5dccaf6b4ee8d7affab889fbf
mjsvijit <viratsinghfzd75@gmail.com>
2022-03-14 21:40:25 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import { Vegitables } from './Components/Vegitables';
+import { Wishlist } from './Components/Wishlist';
+
+function App() {
+  return (
+    <div >
+     <Wishlist />
+      <Vegitables /> 
+    </div>
+  );
+}
+
+export default App;
